bc26cf7fe08e8d5a40bf241eadd17ac52d0c9653
Sesan <148628462+1esan@users.noreply.github.com>
2024-05-16 00:14:35 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,41 @@
+#ifndef INCLUDE_AUTOMATA_H_
+#define INCLUDE_AUTOMATA_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+class Automata {
+ public:
+    Automata();
+    enum State {
+        OFF,
+        WAIT,
+        ACCEPT,
+        CHECK,
+        COOK
+    };
+    void on();
+    void off();
+    void coin(int money);
+    void printMenu();
+    void choice(int item);
+    void cancel();
+    void cook();
+    void finish();
+    Automata::State getState();
+    int getCash();
+
+ private:
+    State state;
+    vector<string> menu;
+    vector<int> prices;
+    int cash;
+    int choice_;
+    int current_price;
+};
