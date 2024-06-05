578542388044f4b4930b95bee829f3958d5678bb
Sesan <148628462+1esan@users.noreply.github.com>
2024-06-05 15:38:54 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,68 +1,104 @@
-// Copyright 2022 GHA Test Team
+// Copyright 2024 UNN-IASR
 
 #include <gtest/gtest.h>
 #include "Automata.h"
 
-TEST(states, ON) {
-  Automata machine = Automata();
-  machine.on();
-  EXPECT_EQ(Automata::WAIT, machine.getState());
+TEST(automataTest, createAutomataIsInvalid) {
+    EXPECT_THROW(Automata automata({ "Latte", "Espresso" }, { 100, 80, 90 }),
+        std::invalid_argument);
 }
 
-TEST(states, OFF) {
-  Automata machine = Automata();
-  machine.on();
-  machine.off();
-  EXPECT_EQ(Automata::OFF, machine.getState());
+TEST(automataTest, onIfAutomataIsOFF) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    EXPECT_EQ(automata.getState(), "Waiting");
 }
 
-TEST(states, ACCEPT) {
-  Automata machine = Automata();
-  machine.on();
-  machine.coin(10);
-  EXPECT_EQ(Automata::ACCEPT, machine.getState());
+TEST(automataTest, onIfAutomataIsON) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.coin(100);
+    automata.on();
+    EXPECT_EQ(automata.getState(), "Accepting money");
 }
 
-TEST(states, CANCEL) {
-  Automata machine = Automata();
-  machine.on();
-  machine.coin(10);
-  machine.cancel();
-  EXPECT_EQ(Automata::WAIT, machine.getState());
+TEST(automataTest, offIsValid) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.off();
+    EXPECT_EQ(automata.getState(), "OFF");
 }
 
-
-TEST(states, CHECK) {
-  Automata machine = Automata();
-  machine.on();
-  machine.coin(10);
-  machine.choice(1);
-  EXPECT_EQ(Automata::CHECK, machine.getState());
+TEST(automataTest, offIsInvalid) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.coin(100);
+    EXPECT_THROW(automata.off(), std::logic_error);
 }
 
-TEST(states, COOK) {
-  Automata machine = Automata();
-  machine.on();
-  machine.coin(10);
-  machine.choice(1);
-  machine.cook();
-  EXPECT_EQ(Automata::COOK, machine.getState());
+TEST(automataTest, coinIsValid) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.coin(100);
+    EXPECT_EQ(automata.getState(), "Accepting money");
 }
 
-TEST(states, FINISH) {
-  Automata machine = Automata();
-  machine.on();
-  machine.coin(10);
-  machine.choice(1);
-  machine.cook();
-  machine.finish();
-  EXPECT_EQ(Automata::WAIT, machine.getState());
+TEST(automataTest, coinIfIncorrectMoney) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    EXPECT_THROW(automata.coin(-10), std::invalid_argument);
 }
 
-TEST(balanceTest, CASH) {
-  Automata machine = Automata();
-  machine.on();
-  machine.coin(10);
-  machine.getCash();
-  EXPECT_EQ(10, machine.getCash());
+TEST(automataTest, coinIfIncorrectState) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    EXPECT_THROW(automata.coin(100), std::logic_error);
+}
+
+TEST(automataTest, choiceIsValid) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.coin(100);
+    automata.choice(3);
+    EXPECT_EQ(automata.getState(), "Waiting");
+}
+
+TEST(automataTest, choiceIfIncorrectIndex) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.coin(100);
+    EXPECT_THROW(automata.choice(-1), std::invalid_argument);
+}
+
+TEST(automataTest, choiceIfIncorrectState) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    EXPECT_THROW(automata.choice(-1), std::invalid_argument);
+}
+
+TEST(automataTest, choiceIfIsNotEnoughMoney) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.coin(10);
+    automata.choice(1);
+    EXPECT_EQ(automata.getState(), "Waiting");
+}
+
+TEST(automataTest, cancelIsValid) {
+    Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" },
+        { 100, 80, 90, 75 });
+    automata.on();
+    automata.coin(50);
+    automata.cancel();
+    EXPECT_EQ(automata.getState(), "Waiting");
 }
